7ed994365aa26714b73381cf2622ad0c4b64ea4d
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-02-08 01:22:18 +0530

    Remove duplicate code from overlay driver
    Remove duplicate code from overlay driver.
    
    Change-Id: I296329a6f86686e1da37c228f9afafca7ef62bed
    Issue-Id: SDNHUB-45
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ service/src/integration-test/java/org/openo/sdnhub/overlayvpndriver/test/mocoserver/IpSecDriverHttpsServiceServer.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package org.openo.sdnhub.overlayvpndriver.test.mocoserver;
+
+import java.util.List;
+
+import org.codehaus.jackson.type.TypeReference;
+import org.openo.sdno.framework.container.util.JsonUtil;
+import org.openo.sdno.overlayvpn.errorcode.ErrorCode;
+import org.openo.sdno.overlayvpn.result.ResultRsp;
+import org.openo.sdnhub.overlayvpndriver.login.OverlayVpnDriverResponse;
+import org.openo.sdnhub.overlayvpndriver.model.ipsec.adapter.NetIpSecModel;
+import org.openo.sdno.testframework.http.model.HttpResponse;
+import org.openo.sdno.testframework.http.model.HttpRquestResponse;
+import org.openo.sdno.testframework.moco.MocoHttpsServer;
+import org.openo.sdno.testframework.moco.responsehandler.MocoResponseHandler;
+
+public class IpSecDriverHttpsServiceServer extends MocoHttpsServer {
+
+    public IpSecDriverHttpsServiceServer() {
+        super();
+    }
+
+    @Override
+    public void addRequestResponsePairs() {
+
+        this.addRequestResponsePair("src/integration-test/resources/overlayvpndriver/moco/ipsecCreateSuccess.json",
+                new IpSecSuccessResponseHandler());
+        this.addRequestResponsePair("src/integration-test/resources/overlayvpndriver/moco/ipsecCreatePutSuccess.json",
+                new IpSecSuccessResponseHandler());
+        this.addRequestResponsePair("src/integration-test/resources/overlayvpndriver/moco/ESRGetController.json",
+                new MocoResponseHandler());
+    }
+
+    private class IpSecSuccessResponseHandler extends MocoResponseHandler {
+
+        @Override
+        public void processRequestandResponse(HttpRquestResponse httpObject) {
+
+            // HttpRequest httpRequest = httpObject.getRequest();
+            HttpResponse httpResponse = httpObject.getResponse();
+
+            OverlayVpnDriverResponse<List<NetIpSecModel>> inputInstanceList = JsonUtil.fromJson(httpResponse.getData(),
+                    new TypeReference<OverlayVpnDriverResponse<List<NetIpSecModel>>>() {});
+            ResultRsp<OverlayVpnDriverResponse<List<NetIpSecModel>>> newResult =
+                    new ResultRsp<OverlayVpnDriverResponse<List<NetIpSecModel>>>(ErrorCode.OVERLAYVPN_SUCCESS);
+            httpResponse.setStatus(200);
+            newResult.setData(inputInstanceList);
+            httpResponse.setData(JsonUtil.toJson(inputInstanceList));
+        }
+    }
+}
---
+++ service/src/integration-test/java/org/openo/sdnhub/overlayvpndriver/test/mocoserver/WanInterfaceDriverHttpsServer.java
@@ -0,0 +1,62 @@
+/*******************************************************************************
+ * Copyright 2016-2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
+
+package org.openo.sdnhub.overlayvpndriver.test.mocoserver;
+
+import java.util.List;
+
+import org.codehaus.jackson.type.TypeReference;
+import org.openo.sdno.framework.container.util.JsonUtil;
+import org.openo.sdno.overlayvpn.errorcode.ErrorCode;
+import org.openo.sdno.overlayvpn.model.port.WanSubInterface;
+import org.openo.sdno.overlayvpn.result.ResultRsp;
+import org.openo.sdnhub.overlayvpndriver.login.OverlayVpnDriverResponse;
+import org.openo.sdno.testframework.http.model.HttpRequest;
+import org.openo.sdno.testframework.http.model.HttpResponse;
+import org.openo.sdno.testframework.http.model.HttpRquestResponse;
+import org.openo.sdno.testframework.moco.MocoHttpsServer;
+import org.openo.sdno.testframework.moco.responsehandler.MocoResponseHandler;
+
+public class WanInterfaceDriverHttpsServer extends MocoHttpsServer{
+    public WanInterfaceDriverHttpsServer() {
+        super();
+    }
+    @Override
+    public void addRequestResponsePairs() {
+
+        this.addRequestResponsePair("src/integration-test/resources/overlayvpndriver/moco/queryWanInterfaceSuccess.json",
+                new wanInterfaceResponseHandler());
+        this.addRequestResponsePair("src/integration-test/resources/overlayvpndriver/moco/ESRGetController.json",
+                new MocoResponseHandler());
+    }
+
+    private class wanInterfaceResponseHandler extends MocoResponseHandler {
+
+        @Override
+        public void processRequestandResponse(HttpRquestResponse httpObject) {
+
+            HttpRequest httpRequest = httpObject.getRequest();
+            HttpResponse httpResponse = httpObject.getResponse();
+            OverlayVpnDriverResponse<List<WanSubInterface>> inputInstanceList = JsonUtil.fromJson(httpResponse.getData(),
+                    new TypeReference<OverlayVpnDriverResponse<List<WanSubInterface>>>() {});
+            ResultRsp<OverlayVpnDriverResponse<List<WanSubInterface>>> newResult =
+                    new ResultRsp<OverlayVpnDriverResponse<List<WanSubInterface>>>(ErrorCode.OVERLAYVPN_SUCCESS);
+            httpResponse.setStatus(200);
+            newResult.setData(inputInstanceList);
+            httpResponse.setData(JsonUtil.toJson(inputInstanceList));
+        }
+    }
+}
