79c1c9d0e499799c47ec446e2dae7f5dc2c35e2e
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-02-13 11:45:20 +0530

    Static Route interface for overlay driver
    Static Route interface for overlaydriver.
    
    Issue-Id: SDNHUB-50
    Change-Id: I36a07b36f6a1ccf4ed0170f2428e6718afc65313
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

---
+++ service/src/main/java/org/openo/sdnhub/overlayvpndriver/controller/model/ControllerNbiStaticRoute.java
@@ -0,0 +1,164 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdnhub.overlayvpndriver.controller.model;
+
+/**
+ * Model class from ControllerNbiStaticRoute<br/>
+ *
+ * @author
+ * @version SDNHUB 0.5 02-Feb-2017
+ */
+public class ControllerNbiStaticRoute {
+
+    private String id;
+
+    private String bfdName;
+
+    private String description;
+
+    private String priority;
+
+    private String vpnName;
+
+    private String outInterface;
+
+    private String nextHop;
+
+    private boolean dhcp;
+
+    private String vpnId;
+
+    private String mask;
+
+    private String nqaId;
+
+    private String ip;
+
+    private String ipv6Address;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getBfdName() {
+        return bfdName;
+    }
+
+    public void setBfdName(String bfdName) {
+        this.bfdName = bfdName;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getVpnName() {
+        return vpnName;
+    }
+
+    public void setVpnName(String vpnName) {
+        this.vpnName = vpnName;
+    }
+
+    public String getOutInterface() {
+        return outInterface;
+    }
+
+    public void setOutInterface(String outInterface) {
+        this.outInterface = outInterface;
+    }
+
+    public String getNextHop() {
+        return nextHop;
+    }
+
+    public void setNextHop(String nextHop) {
+        this.nextHop = nextHop;
+    }
+
+    public boolean getDhcp() {
+        return dhcp;
+    }
+
+    public void setDhcp(boolean dhcp) {
+        this.dhcp = dhcp;
+    }
+
+    public String getVpnId() {
+        return vpnId;
+    }
+
+    public void setVpnId(String vpnId) {
+        this.vpnId = vpnId;
+    }
+
+    public String getMask() {
+        return mask;
+    }
+
+    public void setMask(String mask) {
+        this.mask = mask;
+    }
+
+    public String getNqaId() {
+        return nqaId;
+    }
+
+    public void setNqaId(String nqaId) {
+        this.nqaId = nqaId;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getIpv6Address() {
+        return ipv6Address;
+    }
+
+    public String setIpv6Address(String ipv6address) {
+        return this.ipv6Address = ipv6address;
+    }
+
+    @Override
+    public String toString() {
+        return "ClassPojo [id = " + id + ", bfdName = " + bfdName + ", description = " + description + ", priority = "
+                + priority + ", vpnName = " + vpnName + ", outInterface = " + outInterface + ", nextHop = " + nextHop
+                + ", dhcp = " + dhcp + ", vpnId = " + vpnId + ", mask = " + mask + ", nqaId = " + nqaId + ", ip = " + ip
+                + "]";
+    }
+}
---
+++ service/src/main/java/org/openo/sdnhub/overlayvpndriver/sbi/impl/StaticRouteImpl.java
@@ -0,0 +1,254 @@
+/*
+ * Copyright 2017 Huawei Technologies Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openo.sdnhub.overlayvpndriver.sbi.impl;
+
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.apache.commons.collections.CollectionUtils;
+import org.codehaus.jackson.type.TypeReference;
+import org.openo.baseservice.remoteservice.exception.ServiceException;
+import org.openo.sdnhub.overlayvpndriver.controller.consts.ControllerUrlConst;
+import org.openo.sdnhub.overlayvpndriver.controller.model.ControllerNbiStaticRoute;
+import org.openo.sdnhub.overlayvpndriver.http.OverlayVpnDriverProxy;
+import org.openo.sdnhub.overlayvpndriver.result.ACDelResponse;
+import org.openo.sdnhub.overlayvpndriver.result.OverlayVpnDriverResponse;
+import org.openo.sdnhub.overlayvpndriver.translator.StaticRouteConvert;
+import org.openo.sdno.framework.container.util.JsonUtil;
+import org.openo.sdno.overlayvpn.errorcode.ErrorCode;
+import org.openo.sdno.overlayvpn.result.ResultRsp;
+import org.openo.sdno.util.http.HTTPReturnMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
+
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * SBI Implementation of staticRoute services.<br/>
+ *
+ * @author
+ * @version SDNHUB 0.5 02-Feb-2017
+ */
+public class StaticRouteImpl {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(StaticRouteImpl.class);
+
+    private static final String DELETE_ROUTE_PARAMETER = "ids";
+
+    private static final String DEST_IP_QUERY_PARAM = "?destIp=";
+
+    /**
+     * Query static routes<br/>
+     *
+     * @param ctrlUuid Controller UUID
+     * @param deviceId Device UUID
+     * @param destIp Destination IP
+     * @return ResultRsp object with static route list data.
+     * @throws ServiceException ServiceException In case of any query exception
+     * @since SDNHUB 0.5
+     */
+    public ResultRsp<List<ControllerNbiStaticRoute>> queryRouteByDevice(String ctrlUuid, String deviceId,
+                                                                        String destIp, String staticRouteId)
+            throws ServiceException {
+        ResultRsp<List<ControllerNbiStaticRoute>> resultRsp =
+                new ResultRsp<List<ControllerNbiStaticRoute>>(ErrorCode.OVERLAYVPN_SUCCESS);
+
+        String url = MessageFormat.format(ControllerUrlConst.CONST_CONFIG_STATICROUTE, deviceId);
+
+        boolean appendFlg = false;
+        if(StringUtils.hasLength(destIp)) {
+            StringBuilder builder = new StringBuilder();
+            builder.append(url + DEST_IP_QUERY_PARAM + destIp);
+            url = builder.toString();
+            appendFlg = true;
+        }
+
+        if (StringUtils.hasLength(staticRouteId))
+        {
+            String prefix = appendFlg ? "&staticRouteId=" : "?staticRouteId=";
+
+            StringBuilder strBuidler = new StringBuilder();
+            strBuidler.append(url + prefix + staticRouteId);
+            url = strBuidler.toString();
+        }
+
+
+
+        HTTPReturnMessage httpMsg = OverlayVpnDriverProxy.getInstance().sendGetMsg(url, null, ctrlUuid);
+        String body = httpMsg.getBody();
+
+        if(httpMsg.isSuccess() && StringUtils.hasLength(body)) {
+            OverlayVpnDriverResponse<List<ControllerNbiStaticRoute>> response = JsonUtil.fromJson(body,
+                    new TypeReference<OverlayVpnDriverResponse<List<ControllerNbiStaticRoute>>>() {});
+
+            if(response != null && response.isSucess()) {
+                resultRsp.setData(response.getData());
+                return resultRsp;
+            }
+
+            LOGGER.error("Query route By device:ac response return error");
+            throw new ServiceException(ErrorCode.ADAPTER_ROUTER_RESPONSE_FAIL);
+        }
+
+        LOGGER.error("configstatic route failed");
+        return new ResultRsp<>(ErrorCode.ADAPTER_ROUTER_RESPONSE_FAIL);
+
+    }
+
+    /**
+     * Create or update static routes<br/>
+     *
+     * @param ctrlUuid Controller UUID
+     * @param deviceId Device UUID
+     * @param list List of ControllerNbiStaticRoute objet to be created or updated.
+     * @param createOrUpdate True if static route to be created else false if static route to be updated
+     * @return ResultRsp object with static route list data.
+     * @throws ServiceException ServiceException In case of any create or update exception
+     * @since SDNHUB 0.5
+     */
+    public ResultRsp<List<ControllerNbiStaticRoute>> configStaticRoute(String ctrlUuid, String deviceId,
+            List<ControllerNbiStaticRoute> list, boolean createOrUpdate) throws ServiceException {
+
+        ResultRsp<List<ControllerNbiStaticRoute>> resultRsp =
+                new ResultRsp<List<ControllerNbiStaticRoute>>(ErrorCode.OVERLAYVPN_SUCCESS);
+
+        StaticRouteConvert.filterCreatedStaticRouteList(ctrlUuid, deviceId, list);
+
+        List<ControllerNbiStaticRoute> existingRoutes = new ArrayList<>();
+        if(createOrUpdate) {
+            existingRoutes = filterAcExistStaticRouteList(ctrlUuid, deviceId, list);
+        }
+
+        if(CollectionUtils.isEmpty(list)) {
+            resultRsp.setData(existingRoutes);
+            return resultRsp;
+        }
+
+        Map<String, List<ControllerNbiStaticRoute>> reqMap = new HashMap<String, List<ControllerNbiStaticRoute>>();
+        reqMap.put("staticRouteConfigs", list);
+        String url = MessageFormat.format(ControllerUrlConst.CONST_CONFIG_STATICROUTE, deviceId);
+
+        HTTPReturnMessage httpMsg =
+                OverlayVpnDriverProxy.getInstance().sendPutMsg(url, JsonUtil.toJson(reqMap), ctrlUuid);
+
+        String body = httpMsg.getBody();
+
+        if(httpMsg.isSuccess() && StringUtils.hasLength(body)) {
+            OverlayVpnDriverResponse<List<ControllerNbiStaticRoute>> acresponse = JsonUtil.fromJson(body,
+                    new TypeReference<OverlayVpnDriverResponse<List<ControllerNbiStaticRoute>>>() {});
+            if(!acresponse.isSucess()) {
+                return new ResultRsp<List<ControllerNbiStaticRoute>>(ErrorCode.ADAPTER_ROUTER_RESPONSE_FAIL,
+                        acresponse.getErrmsg(), null, null, null);
+            }
+            resultRsp.setData(acresponse.getData());
+            filldata(resultRsp, existingRoutes);
+            return resultRsp;
+        }
+
+        LOGGER.error("configstatic route failed");
+        return new ResultRsp<>(ErrorCode.ADAPTER_ROUTER_RESPONSE_FAIL);
+
+    }
+
+    private void filldata(ResultRsp<List<ControllerNbiStaticRoute>> resultRsp,
+            List<ControllerNbiStaticRoute> existingRoutes) {
+        if(CollectionUtils.isNotEmpty(existingRoutes)) {
+            List<ControllerNbiStaticRoute> rspData = resultRsp.getData();
+            if(CollectionUtils.isEmpty(rspData)) {
+                resultRsp.setData(new ArrayList<ControllerNbiStaticRoute>());
+            }
+            rspData.addAll(existingRoutes);
+        }
+
+    }
+
+    private List<ControllerNbiStaticRoute> filterAcExistStaticRouteList(String ctrlUuid, String deviceId,
+            List<ControllerNbiStaticRoute> sbiStaticRoutes) throws ServiceException {
+        List<ControllerNbiStaticRoute> duplicateRoutes = new ArrayList<ControllerNbiStaticRoute>();
+
+        ResultRsp<List<ControllerNbiStaticRoute>> staticRouteListRsp = queryRouteByDevice(ctrlUuid, deviceId, null, null);
+
+        if(CollectionUtils.isEmpty(staticRouteListRsp.getData())) {
+            return duplicateRoutes;
+        }
+
+        List<ControllerNbiStaticRoute> existingAllRoutingList = staticRouteListRsp.getData();
+        List<ControllerNbiStaticRoute> routesToIgnore = new ArrayList<ControllerNbiStaticRoute>();
+
+        for(int i = (sbiStaticRoutes.size() - 1); i >= 0; i--) {
+            ControllerNbiStaticRoute tempCreateStaticRoute = sbiStaticRoutes.get(i);
+            boolean isCreated = false;
+
+            ControllerNbiStaticRoute similarStaticRoute =
+                    StaticRouteConvert.getSameTunnelFromAc(existingAllRoutingList, tempCreateStaticRoute);
+
+            if(null != similarStaticRoute) {
+                isCreated = true;
+            }
+
+            if(isCreated) {
+                duplicateRoutes.add(similarStaticRoute);
+                routesToIgnore.add(tempCreateStaticRoute);
+            }
+        }
+
+        if(CollectionUtils.isNotEmpty(routesToIgnore)) {
+            sbiStaticRoutes.removeAll(routesToIgnore);
+        }
+        return duplicateRoutes;
+    }
+
+    /**
+     * Delete static routes<br/>
+     *
+     * @param ctrlUuid Controller UUID
+     * @param deviceId Device UUID
+     * @param idList
+     * @return ResultRsp Object for deleted static routes.
+     * @throws ServiceException ServiceException In case of any delete exception
+     * @since SDNHUB 0.5
+     */
+    public ResultRsp<String> deleteRouteByDevice(String ctrlUuid, String deviceId, List<String> idList)
+            throws ServiceException {
+        ResultRsp<String> resultRsp = new ResultRsp<String>(ErrorCode.OVERLAYVPN_SUCCESS);
+        Map<String, List<String>> reqMap = new HashMap<String, List<String>>();
+        reqMap.put(DELETE_ROUTE_PARAMETER, idList);
+        String url = MessageFormat.format(ControllerUrlConst.CONST_CONFIG_STATICROUTE, deviceId);
+
+        HTTPReturnMessage httpMsg =
+                OverlayVpnDriverProxy.getInstance().sendDeleteMsg(url, JsonUtil.toJson(reqMap), ctrlUuid);
+
+        String body = httpMsg.getBody();
+
+        if(httpMsg.isSuccess() && StringUtils.hasLength(body)) {
+            ACDelResponse acdelReponse = JsonUtil.fromJson(body, new TypeReference<ACDelResponse>() {});
+            if(!acdelReponse.isSucess()) {
+                resultRsp.setErrorCode(ErrorCode.ADAPTER_ROUTER_RESPONSE_FAIL);
+                resultRsp.setMessage(acdelReponse.getAllErrmsg());
+            }
+            return resultRsp;
+        }
+
+        LOGGER.error("configstatic route failed");
+        resultRsp.setErrorCode(ErrorCode.OVERLAYVPN_FAILED);
+        resultRsp.setMessage("delete Route by device failed.");
+        return resultRsp;
+    }
+}
