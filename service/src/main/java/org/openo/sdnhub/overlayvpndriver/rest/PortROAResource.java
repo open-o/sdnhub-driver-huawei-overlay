0d091606b6f007e807875ef3ca0e7f582e2a2a5f
subhash kumar singh <subhash.kumar.singh@huawei.com>
2017-02-25 12:13:13 +0530

    Typo fix for PortROA interface
    Typo fix for PortROA interface.
    
    Change-Id: I690997810483ae47a860834efd7da6acb2f0b734
    Issue-Id: SDNHUB-75
    Signed-off-by: subhash kumar singh <subhash.kumar.singh@huawei.com>

--- service/src/main/java/org/openo/sdnhub/overlayvpndriver/rest/PortROAResource.java
+++ service/src/main/java/org/openo/sdnhub/overlayvpndriver/rest/PortROAResource.java
@@ -76,7 +76,7 @@ public class PortROAResource {
      * @since SDNHUB 0.5
      */
     @GET
-    @Path("/device/{deviceid}/ports")
+    @Path("/device/{deviceId}/ports")
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public ResultRsp<SbiIp> queryPortIpByPortName(@Context HttpServletRequest request,
